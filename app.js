bfa762dc457b18821b0c6257bf0dd3c8880ceee3
AJ Kausha <kausha2923@gmail.com>
2019-11-07 14:59:29 +0100

    New Update

---
+++ app.js
@@ -0,0 +1,23 @@
+//FANCYBOX(AND OUR MOBILE MENU)
+$(function () {
+  $('.trigger').on('click', function (e) {
+    e.preventDefault();
+    $('body').toggleClass('menu-is-open');
+  });
+  buttons: [
+    // "zoom",
+    //"share",
+    // "slideShow",
+    //"fullScreen",
+    //"download",
+    // "thumbs",
+    "close"
+    
+  ],
+  $('[data-fancybox="gallery"]').fancybox({
+   loop:true,
+   animationEffect: "zoom",
+   animationDuration: 366,
+  });
+
+});
